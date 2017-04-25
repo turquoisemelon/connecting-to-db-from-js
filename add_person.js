e585887a5994b7783673c6f868e48ac2c3146e6e
Meltem <melltemkilic@gmail.com>
2017-04-25 21:25:37 +0000

    exercise to get used to using knex

---
+++ add_person.js
@@ -0,0 +1,26 @@
+const settings = require("./settings"); // settings.json
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user     : settings.user,
+    password : settings.password,
+    database : settings.database,
+    host     : settings.hostname,
+    port     : settings.port,
+  }
+});
+
+const firstName = process.argv[2];
+const lastName = process.argv[3];
+const birthDate = process.argv[4];
+
+knex.select('*').from('famous_people').then(function (result) {
+
+  knex.insert({first_name: `${firstName}`, last_name: `${lastName}`, birthdate: `${birthDate}`}).into('famous_people') // do a second query
+
+  console.log(result);
+
+}).catch(function (err) {
+  if (err) { console.error(err);;}
+});
